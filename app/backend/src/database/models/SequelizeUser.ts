bdd5894ea362a893a4f9cdec134a1db86a4af612
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-08-06 21:21:47 -0300

    resolvendo identacao

--- app/backend/src/database/models/SequelizeUser.ts
+++ app/backend/src/database/models/SequelizeUser.ts
@@ -19,30 +19,30 @@ InferCreationAttributes<SequelizeUser>> {
 
   declare password: string;
 }
- 
+
 SequelizeUser.init({
   id: {
     type: DataTypes.INTEGER,
     allowNull: false,
     primaryKey: true,
     autoIncrement: true,
-},
+  },
   username: {
     type: DataTypes.STRING(30),
     allowNull: false,
-},
+  },
   role: {
     type: DataTypes.STRING(30),
     allowNull: false,
-},
+  },
   email: {
     type: DataTypes.STRING(50),
     allowNull: false,
-},
+  },
   password: {
-  type: DataTypes.STRING(50),
+    type: DataTypes.STRING(50),
 },
-  }, {
+}, {
   sequelize: db,
   modelName: 'users',
   timestamps: false,

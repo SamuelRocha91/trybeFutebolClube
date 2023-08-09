7e6946d297559fb86551c0c39b845acaa2f862d6
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-08-09 09:44:57 -0300

    requisito 10

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -9,10 +9,10 @@ class Validations {
     const emailRegex = /\S+@\S+\.\S+/;
     // https://horadecodar.com.br/expressao-regular-para-validar-e-mail-javascript-regex/
     if (!emailRegex.test(login.email)) {
-      return res.status(400).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
     if (login.password.length <= 6) {
-      return res.status(400).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
     next();
   }
